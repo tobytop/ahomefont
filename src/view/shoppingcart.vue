d9286e9f005ec4b1f3506b1c0b9997d784542cb5
chw <homelea@163.com>
2020-05-18 09:53:48 +0800

    修改代码

--- src/view/shoppingcart.vue
+++ src/view/shoppingcart.vue
@@ -1,6 +1,10 @@
 <template scoped>
   <div>
-    <van-swipe-cell ref="productitem">
+    <van-swipe-cell
+      v-for="(item, key) in productlist"
+      ref="productitem"
+      :key="key"
+    >
       <van-row type="flex" justify="center" style="background-color: #fafafa">
         <van-col class="selectitem" span="3">
           <van-checkbox v-model="checked" />
@@ -11,18 +15,23 @@
             origin-price="1.00"
             price="2.00"
             desc="息描述描述信息述信息息描述描述信息述信息息描述描述信息述信息息描述描述信息述信息息描述描述信息述信息息描述描述信息述信息息描述描述信息述信息息描述描述信息述信息"
-            title="商品标题"
+            :title="'商品标题' + item"
             thumb="https://img.yzcdn.cn/vant/ipad.jpeg"
           >
             <template #num>
               <span style="font-size:14px;">共计:</span>
-              <span style="font-size:12px;margin-left:4px;color:#ee0a24;">¥</span>
+              <span style="font-size:12px;margin-left:4px;color:#ee0a24;"
+                >¥</span
+              >
               <span style="font-size:12px;color:#ee0a24;">
                 <span style="font-size:16px;">20</span>
-                .00</span>
+                .00</span
+              >
             </template>
             <template #price-top>
-              <div class="more"><van-icon name="weapp-nav" @click.stop="showdelete" /></div>
+              <div class="more">
+                <van-icon name="weapp-nav" @click.stop="showdelete(key)" />
+              </div>
             </template>
           </van-card>
         </van-col>
@@ -30,9 +39,8 @@
       <template #right>
         <van-button square text="删除" type="danger" class="delete-button" />
       </template>
-  </van-swipe-cell>
-
-    
+    </van-swipe-cell>
+    <van-divider style="margin-bottom: 110px;">已经到底了</van-divider>
     <van-submit-bar
       style="bottom:52px;"
       :price="3050"
@@ -48,42 +56,46 @@
 export default {
   data() {
     return {
-      checked: true
+      checked: true,
+      productlist: ["a", "b", "c", "d", "e", "f"]
     };
   },
   methods: {
-    onclick(e){
+    onclick(e) {
       console.log(e);
     },
     onSubmit() {
-      this.$router.push("/orderpage")
+      this.$router.push("/orderpage");
     },
-    showdelete(){
-      //console.log(this.$refs.productitem);
-      this.$refs.productitem.open('right');
-    },
+    showdelete(key) {
+      //console.log(key);
+      this.$refs.productitem[key].open("right");
+    }
   }
 };
 </script>
 <style scoped>
-.van-card{
+.van-divider {
+  margin: 5px 0px;
+}
+.van-card {
   padding-left: 5px;
 }
 .delete-button {
-    height: 100%;
+  height: 100%;
 }
-.more{
+.more {
   position: absolute;
   right: 0px;
   top: 35%;
   font-size: 1rem;
 }
-.van-card__desc{
+.van-card__desc {
   width: 90%;
 }
-.selectitem{
+.selectitem {
   margin: auto;
-  padding-left: 10px; 
+  padding-left: 10px;
   position: relative;
   left: 15px;
   z-index: 2;
